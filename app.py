a9129dbb6c21af7fd73a193752f864067d344838
rse112 <chohy121212@naver.com>
2024-03-09 17:36:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-import streamlit as st
+# First, import the elements you need
 
-st.title("Welcome to My Streamlit Homepage!")
+from streamlit_elements import elements, mui, html
 
-st.write(
-    "This is a simple Streamlit Webpage. You can add more interactive elements here.zzzzz"
-)
+# Create a frame where Elements widgets will be displayed.
+#
+# Elements widgets will not render outside of this frame.
+# Native Streamlit widgets will not render inside this frame.
+#
+# elements() takes a key as parameter.
+# This key can't be reused by another frame or Streamlit widget.
 
+with elements("new_element"):
 
-# 사용자 입력 받기
-user_input = st.text_input("Enter your name:")
+    # Let's create a Typography element with "Hello world" as children.
+    # The first step is to check Typography's documentation on MUI:
+    # https://mui.com/components/typography/
+    #
+    # Here is how you would write it in React JSX:
+    #
+    # <Typography>
+    #   Hello world
+    # </Typography>
 
-# 버튼 추가
-if st.button("Say Hello"):
-    st.write(f"Hello, {user_input}!")
+    mui.Typography("Hello world")
