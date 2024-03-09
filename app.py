79f37a85bc3a6b1452715e9c3b4f85d2d7e1edec
rse112 <chohy121212@naver.com>
2024-03-09 21:02:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-# my_app.py
+from st_vizzu import *
+import pandas as pd
 import streamlit as st
-from streamlit_elements import elements, mui
 
+# Load Data
+df = pd.read_csv("test/music_data.csv", index_col=0)
+# Create ipyvizzu Object with the DataFrame
+obj = create_vizzu_obj(df)
 
-# Streamlit 애플리케이션의 메인 페이지를 정의합니다.
-def main():
-    with elements("new_element"):  # elements 프레임 생성
-        mui.Typography(
-            "Hello world"
-        )  # "Hello world" 텍스트를 표시하는 Typography 컴포넌트
+# Preset plot usage. Preset plots works directly with DataFrames.
+bar_obj = bar_chart(
+    df, x="Kinds", y="Popularity", title="1.Using preset plot function `bar_chart()`"
+)
 
+# Animate with defined arguments
+anim_obj = beta_vizzu_animate(
+    bar_obj,
+    x="Genres",
+    y=["Popularity", "Kinds"],
+    title="Animate with beta_vizzu_animate () function",
+    label="Popularity",
+    color="Genres",
+    legend="color",
+    sort="byValue",
+    reverse=True,
+    align="center",
+    split=False,
+)
 
-# 스크립트가 직접 실행될 때만 main 함수를 호출합니다.
-main()
+# Animate with general dict based arguments
+_dict = {
+    "size": {"set": "Popularity"},
+    "geometry": "circle",
+    "coordSystem": "polar",
+    "title": "Animate with vizzu_animate () function",
+}
+anim_obj2 = vizzu_animate(anim_obj, _dict)
+
+# Visualize within Streamlit
+with st.container():  # Maintaining the aspect ratio
+    st.button("Animate")
+    vizzu_plot(anim_obj2)
